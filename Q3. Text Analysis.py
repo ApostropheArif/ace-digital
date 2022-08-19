7b3f50bb8b7cf84d7d505bdfa71df2921375d90c
ApostropheArif <budding.life@gmail.com>
2022-08-20 00:11:00 +0800

    Minor touch-up of the code

--- Q3. Text Analysis.py
+++ Q3. Text Analysis.py
@@ -17,8 +17,7 @@ for sentence in sentences:
 
 print(f"""
 Analysis 1:
-The probability of the word "data" occurring in each line is {round(data_sentences_count/total_sentences, 2)}.
-""")
+The probability of the word "data" occurring in each line is {round(data_sentences_count/total_sentences, 2)}.""")
 
 # Q2: What is the distribution of distinct word counts across all the lines?
 distinct_words_count = []
@@ -30,8 +29,7 @@ for sentence in sentences:
 
 print("""
 Analysis 2:
-The plot show the distribution of distinct word count across all the sentences:
-""")
+The plot shows the distribution of distinct word counts across all the sentences:""")
 fig, ax = plt.subplots()
 sns.histplot(distinct_words_count)
 ax.set_title("Number of unique words in each sentence")
@@ -42,7 +40,7 @@ plt.show()
 # Q3. What is the probability of the word "analytics" occurring after the word "data"?
 data_count = len(re.findall("data", text.lower()))
 data_analytics_count = len(re.findall("data analytics", text.lower()))
-print(f'''
+print('''
 Analysis 3:
-The probability of the word "analytics" occurring after the word "data" is {round(data_analytics_count/data_count, 2)}.
-''')
+The probability of the word "analytics" occurring after the word "data" is '''
+f"{round(data_analytics_count/data_count, 2)}.")
